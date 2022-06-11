39acf3e05466d87e0953d600aafb7ff7204e5fcb
Gregor Petrin <gregor@petrin.co>
2022-06-11 06:55:22 +0200

    Enable HTTPS proxy to local websockets for development

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,22 +1,41 @@
 import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as fs from 'fs';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig( ({ command, mode }) => {
+
+  // Load .env files
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Paths to HTTPS certificates can be provided by environment variables HTTPS_KEY and HTTPS_CERT
+  const httpsConfig = (env.HTTPS_KEY && env.HTTPS_CERT) ? {
+    https: {
+      key: fs.readFileSync(env.HTTPS_KEY),
+      cert: fs.readFileSync(env.HTTPS_CERT)
     }
-  },
-  server: {
-    proxy: {
-      './sock': {
-        target: 'ws://localhost:8888',
-        ws: true
+  } : {}
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        'sockjs-client': path.resolve(__dirname, './node_modules/sockjs-client/dist/sockjs.js'),
       }
-    }
+    },
+    server: {
+      ...httpsConfig,
+      proxy: {
+        '/sock': {
+          target: 'ws://127.0.0.1:8888/',
+          ws: true,
+          changeOrigin: true,
+        }
+      }
+    },
   }
 })
