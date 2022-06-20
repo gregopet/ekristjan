d7659c74278ce7bf030eedda7515e407d93befe8
Gregor Petrin <gregor@petrin.co>
2022-06-20 07:13:54 +0200

    Turn on wake lock
    Increases chance of teachers getting notifications - we may make this
    optional!

--- src/frontend/src/components/RootComponent.vue
+++ src/frontend/src/components/RootComponent.vue
@@ -7,10 +7,11 @@
 </template>
 
 <script lang="ts" setup>
-import { useFetch } from "@vueuse/core";
+import {useFetch, useWakeLock} from "@vueuse/core";
 import {pupils} from "@/data";
 import {type Ref, watch} from "vue";
 
+// Load pupils from server
 const { isFinished, error, data } = useFetch("/pupils")
 watch(data as Ref<string>, (newData: string) => {
   const newPupils = JSON.parse(newData) as dto.Pupil[];
@@ -18,4 +19,8 @@ watch(data as Ref<string>, (newData: string) => {
     pupils.push(...newPupils);
   }
 })
+
+// Keep screen turned on because we want those notifications!
+const { isSupported, isActive, request, release } = useWakeLock()
+request('screen')
 </script>
