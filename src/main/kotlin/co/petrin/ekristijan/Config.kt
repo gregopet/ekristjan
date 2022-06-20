f8b325426e9a382b404b4c5733626a73aeaff43a
Gregor Petrin <gregor@petrin.co>
2022-06-20 07:12:38 +0200

    Allow config to be edited beforehand without errors
    Easier to prepare a new deploy

--- src/main/kotlin/co/petrin/ekristijan/Config.kt
+++ src/main/kotlin/co/petrin/ekristijan/Config.kt
@@ -1,11 +1,13 @@
 package co.petrin.ekristijan
 
 import co.petrin.ekristijan.dto.Pupil
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 /**
  * @property port The port on which the API will be listening
  * @property pupils The list of pupils and their classes
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Config(
     val port: Int,
     val pupils: List<Pupil>,
