5481c40bd4398e582d5bfbfc29122ee4077c87db
Gregor Petrin <gregor@petrin.co>
2022-08-11 07:34:00 +0200

    Backend-based logic handling summons now as well
    Needs thorough testing once frontend is brought up to speed

--- src/main/kotlin/co/petrin/ekristijan/DepartureVerticle.kt
+++ src/main/kotlin/co/petrin/ekristijan/DepartureVerticle.kt
@@ -1,25 +1,19 @@
 package co.petrin.ekristijan
 
-import co.petrin.ekristijan.dto.Pupil
-import co.petrin.ekristijan.dto.PushSubscription
-import co.petrin.ekristijan.dto.event.SendPupilEvent
 import co.petrin.ekristijan.security.ACCESS_TOKEN_SCOPE
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.auth.jwt.JWTAuth
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.ext.web.handler.JWTAuthHandler
 import io.vertx.ext.web.handler.StaticHandler
-import kotlinx.coroutines.launch
-import nl.martijndwars.webpush.Notification
 import nl.martijndwars.webpush.PushAsyncService
-import nl.martijndwars.webpush.Urgency
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
 import si.razum.vertx.config.ConfigurableCoroutineVerticle
 import co.petrin.ekristijan.device.registrationHandler
 import co.petrin.ekristijan.departure.departureStateHandler
+import co.petrin.ekristijan.departure.summonHandler
 
 private val LOG = LoggerFactory.getLogger(DepartureVerticle::class.java)
 
@@ -29,6 +23,7 @@ class DepartureVerticle(val jooq: DSLContext, val jwtProvider: JWTAuth) : Config
   lateinit var pushService: PushAsyncService
 
   /** All push targets we want to send to - indexed by the subscription URL */
+  @Deprecated("Use subscriptions stored in the database!")
   val pushSubscriptions = mutableMapOf<String, SubscribedClient>()
 
   lateinit var parsedConfig: Config
@@ -46,58 +41,13 @@ class DepartureVerticle(val jooq: DSLContext, val jwtProvider: JWTAuth) : Config
 
     router.get("/push/key").handler { ctx -> ctx.end(parsedConfig.vapid.publicKey) }
     router.put("/push/subscribe").handler(BodyHandler.create()).coroutineHandler(::registrationHandler)
-    router.post("/pupils/leave").handler(BodyHandler.create()).handler(this::pupilLeaves)
+    router.post("/pupils/leave").handler(BodyHandler.create()).coroutineHandler(::summonHandler)
     router.get("/pupils/:classes?").coroutineHandler { ctx ->
       departureStateHandler(ctx, ctx.pathParam("classes")?.split(",")?.toTypedArray())
     }
     router.route().handler(StaticHandler.create(parsedConfig.frontendDistFolder ?: "src/frontend/dist"))
 
     return router
-  }
-
-  private fun pupilLeaves(ctx: RoutingContext) {
-    val pupil = ctx.body().asJsonObject().mapTo(Pupil::class.java)
-    LOG.info("Pupil ${pupil.name}, ${pupil.fromClass} called to main gate")
-
-    // release request & process the notifications on a background thread
-    ctx.end()
-    launch {
-      try {
-        val payload = JsonObject.mapFrom(SendPupilEvent(pupil.name, pupil.fromClass)).encode()
-        with(pushSubscriptions.values.iterator()) {
-          while (hasNext()) {
-            val (subscription, subData) = next()
-            if (subData.fromClasses.contains(pupil.fromClass)) {
-              LOG.debug("Sending push notification to ${subscription.endpoint}")
-              val response = pushService.send(Notification(subscription, payload, Urgency.HIGH)).get()
-              LOG.trace("Result had status code ${response.statusCode}")
-              if (response.statusCode == 410) {
-                remove()
-                LOG.debug("Push notification no longer available, removing")
-              }
-            }
-          }
-        }
-      } catch (t: Throwable) {
-        LOG.error("Could not send notification", t)
-        ctx.response().setStatusCode(500).end("Could not send notification")
-      }
-    }
-  }
-
-
-  /** Once client subscribes, it will send its details to us here */
-  private fun subscribe(ctx: RoutingContext) {
-    val sub = ctx.body().asJsonObject().mapTo(PushSubscription::class.java)
-
-    if (pushSubscriptions.containsKey(sub.endpoint)) {
-      LOG.debug("Client updating their subscription to classes ${sub.fromClasses.joinToString()} for user ${sub.keys.auth}")
-      pushSubscriptions[sub.endpoint]!!.subscriptionData = sub
-    } else {
-      LOG.debug("Client subscribing to push notifications for classes ${sub.fromClasses.joinToString()} for user ${sub.keys.auth}")
-      pushSubscriptions[sub.endpoint] = SubscribedClient(sub.subscription, sub)
-    }
-    ctx.end()
   }
 
   override suspend fun readConfiguration(conf: JsonObject, forceStatusOutput: Boolean) {
---
+++ src/main/kotlin/co/petrin/ekristijan/departure/summonHandler.kt
@@ -0,0 +1,49 @@
+package co.petrin.ekristijan.departure
+
+import co.petrin.ekristijan.DepartureVerticle
+import co.petrin.ekristijan.db.DepartureQueries
+import co.petrin.ekristijan.db.DeviceQueries
+import co.petrin.ekristijan.dto.Pupil
+import co.petrin.ekristijan.dto.event.SendPupilEvent
+import co.petrin.ekristijan.security.schoolId
+import co.petrin.ekristijan.security.teacherId
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.coroutines.awaitBlocking
+import nl.martijndwars.webpush.Notification
+import nl.martijndwars.webpush.Urgency
+import org.slf4j.LoggerFactory
+import java.time.OffsetDateTime
+
+private val LOG = LoggerFactory.getLogger("DepartureVerticle.summonHandler")
+
+/**
+ * Handles the summoning function - pupil is requested at the door.
+ * Records the summon and notifies all devices registered to listen for that class.
+ * Returns:
+ *  - 403 if the pupil ID doesn't match the teacher's school
+ */
+suspend fun DepartureVerticle.summonHandler(ctx: RoutingContext) {
+    val pupil = ctx.body().asJsonObject().mapTo(Pupil::class.java)
+
+    awaitBlocking {
+        if (DepartureQueries.summonPupil(pupil.id, ctx.teacherId, OffsetDateTime.now(), jooq)) {
+            LOG.info("Teacher ${ctx.teacherId} summoning pupil $pupil")
+            ctx.response().setStatusCode(204).send()
+            val payload = JsonObject.mapFrom(SendPupilEvent(pupil.name, pupil.fromClass)).encode()
+            DeviceQueries.devicesToNotify(ctx.schoolId, pupil.fromClass, jooq).forEach { subscription ->
+                LOG.debug("Sending push notification to ${subscription.endpoint}")
+                val response = pushService.send(Notification(subscription.subscription, payload, Urgency.HIGH)).get()
+                LOG.trace("Result had status code ${response.statusCode}")
+                if (response.statusCode == 410) {
+                    DeviceQueries.unregisterDevice(subscription.endpoint, jooq)
+                    LOG.debug("Push notification no longer available, removing")
+                }
+            }
+        } else {
+            LOG.warn("Teacher ${ctx.teacherId} wanted to summon pupil $pupil from another school")
+            ctx.response().setStatusCode(403).send()
+        }
+    }
+
+}
