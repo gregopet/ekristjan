566a97c41cd67d7e1d64331249448245125da549
Gregor Petrin <gregor@petrin.co>
2022-09-11 08:26:11 +0200

    Change date format to use . not : as separator
    Apparently this is more correct for Slovene

--- src/frontend/src/dateAndTime.ts
+++ src/frontend/src/dateAndTime.ts
@@ -4,13 +4,13 @@ export function stripSeconds(time: string | dto.LocalTime | null) {
     if (!time) return '';
 
     const timeStr = time as string;
-    return DateTime.fromSQL(timeStr).toFormat("H:mm")
+    return DateTime.fromSQL(timeStr).toFormat("H.mm")
 }
 
 export function date2Time(dateTime: string | dto.OffsetDateTime | null) {
     if (!dateTime) return null;
     const time = DateTime.fromISO(dateTime as string);
-    return time.toFormat("H:mm");
+    return time.toFormat("H.mm");
 }
 
 /** Takes the given local time and gets today's date at that local time */
