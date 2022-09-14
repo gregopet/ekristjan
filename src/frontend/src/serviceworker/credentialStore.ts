92222e8ff250a41b6581e23ed6a58627fee6a8d4
Gregor Petrin <gregor@petrin.co>
2022-09-14 21:07:25 +0200

    Make sure null is returned when no token was stored

--- src/frontend/src/serviceworker/credentialStore.ts
+++ src/frontend/src/serviceworker/credentialStore.ts
@@ -16,7 +16,7 @@ export async function storeTokens(tokens: dto.LoginDTO | null) {
 /** Retrieves any stored token from IndexedDB */
 export async function restoreTokens(): Promise<dto.LoginDTO | null> {
     const db = await getDatabase();
-    return db.get(STORE_NAME, TOKEN_KEY) as Promise<dto.LoginDTO | null>;
+    return (await db.get(STORE_NAME, TOKEN_KEY) || null) as Promise<dto.LoginDTO | null>;
 }
 
 function getDatabase(): Promise<IDBPDatabase> {
