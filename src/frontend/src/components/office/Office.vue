ec9937671669c0477b7ff24cd0e7526b51ee15cb
Gregor Petrin <gregor@petrin.co>
2022-09-19 21:22:08 +0200

    Enable filter in backoffice

--- src/frontend/src/components/office/Office.vue
+++ src/frontend/src/components/office/Office.vue
@@ -17,7 +17,7 @@
         </tr>
         <tr class="text-center">
           <th colspan="2">
-            <input class="input-sm" placeholder="Išči...">
+            <input class="input-sm" placeholder="Išči..." v-model="nameFilter" autofocus>
           </th>
           <td>Sam?</td>
           <td>Pon</td>
@@ -29,7 +29,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-if="data" v-for="pupil in data.sort(sorting)">
+        <tr v-if="data && filteredStudents" v-for="pupil in filteredStudents.sort(sorting)">
           <td>{{ pupil.name }}</td>
           <td class="text-center">{{ pupil.clazz }}</td>
           <td class="text-center p-0 text-2xl text-my-green"><span v-if="pupil.leavesAlone">✓</span></td>
@@ -55,14 +55,33 @@
 import {useFetch} from "@vueuse/core";
 import {stripSeconds} from "@/dateAndTime";
 import Header from "@/components/Header.vue";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
 const time = stripSeconds;
+
+/** Fetch the initial list of students */
 const { isFetching, error, data } = useFetch<dto.PupilDTO[]>('/backoffice/pupils').get().json()
 
+/** The filter that filters pupils and classes by name */
+const nameFilter = ref('')
+
+/** The filter, split into parts */
+const filterParts = computed(() => nameFilter.value.split(' ').map( (p) => p.toUpperCase()))
+
+/** Students matching any of the filter parts */
+const filteredStudents = computed<dto.PupilDTO[]>(() => {
+  if (!nameFilter.value) return data.value;
+  else {
+    return data.value.filter((pup: dto.PupilDTO) => {
+      return filterParts.value.some(filterPart =>
+        pup.clazz.toUpperCase().indexOf(filterPart) >= 0 || pup.name.toUpperCase().indexOf(filterPart) >= 0
+      )
+    })
+  }
+})
+
+/** A sorting algorithm for the pupils */
 const sorting = ref((pup1: dto.PupilDTO, pup2: dto.PupilDTO) => {
   return pup1.clazz.localeCompare(pup2.clazz) || pup1.name.localeCompare(pup2.name)
 })
-
-
 </script>
