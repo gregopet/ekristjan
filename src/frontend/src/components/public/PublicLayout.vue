a4c0e38209f80f40d3312d9c6e2d5af0260bc07f
Gregor Petrin <gregor@petrin.co>
2022-10-02 11:04:37 +0200

    Have a white background on mobile layouts (despite the user's theme)
    When we have a dark theme, the background may well be dark, but
    currently everything only is styled for light theme & a dark background
    just messes up the design

--- src/frontend/src/components/public/PublicLayout.vue
+++ src/frontend/src/components/public/PublicLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:flex md:items-center md:h-screen md:min-w-fit md:bg-azure">
+  <div class="md:flex md:items-center md:h-screen md:min-w-fit md:bg-azure bg-white min-h-screen">
     <div class="md:container md:mx-auto md:shadow-2xl md:shrink md:basis-[500px] md:rounded-lg p-8 bg-white">
 
       <div class="font-logo text-3xl text-center text-gray-700 mb-4 tracking-wider">
