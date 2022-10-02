6656a0c551a657c9783c93ae91bb7adfeb3c3185
Gregor Petrin <gregor@petrin.co>
2022-10-02 19:57:48 +0200

    Make it possible for the door person to also mark a pupil as departed
    In case a teacher forgets, this allows the door staff to clear their
    awaiting queue.

--- src/frontend/src/components/door/DoorSummonedPupils.vue
+++ src/frontend/src/components/door/DoorSummonedPupils.vue
@@ -1,18 +1,20 @@
 <template>
   <h5 class="text-xl pt-5 pl-5" v-if="summoned.length">Čakajo na potrditev:</h5>
   <ul class="p-5 mb-10">
-    <li v-for="pupil in summoned" class="flex justify-between">
+    <li v-for="pupil in summoned" class="flex justify-between cursor-pointer" @click="pupilWithDialog = pupil">
       <span>{{ pupil.pupil.name }}</span>
       <span>{{ formatDate(pupil.summon.time) }}</span>
     </li>
   </ul>
+  <PupilDialog v-if="pupilWithDialog" :pupil="pupilWithDialog" @close="pupilWithDialog = null" />
 </template>
 
 <script lang="ts" setup>
 
 import {pupils} from "@/data";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import {date2Time} from "@/dateAndTime";
+import PupilDialog from "@/components/classroom/PupilDialog.vue";
 
 const formatDate = date2Time;
 
@@ -25,4 +27,6 @@ const summoned = computed(() =>
     .filter(pupil => pupil.summon != null && !pupil.departure )
     .map(pupil => pupil as SummonedPupil)
 )
+
+const pupilWithDialog = ref<dto.DailyDeparture | null>(null)
 </script>
