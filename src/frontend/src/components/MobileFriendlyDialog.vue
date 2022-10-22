65695018511a239025c7e35c2aaf7db8330c3150
Gregor Petrin <gregor@petrin.co>
2022-10-22 23:43:25 +0200

    Try preventing "zoom in & don't see modal dialogs" problem users have reported
    Javascript magic for now, let's hope it works well enough.

---
+++ src/frontend/src/components/MobileFriendlyDialog.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div @click.self="close()" class="fixed left-0 right-0 top-0 bottom-0 z-20 hidden sm:block sm:opacity-80 sm:bg-gray-300" ref="dialogBackdrop"></div>
+    <div class="fixed flex flex-col sm:justify-center left-0 right-0 top-0 bottom-0 z-20 bg-white sm:bg-transparent pointer-events-none" ref="dialogParent">
+      <div class="bg-white sm:mx-auto sm:shadow-lg sm:rounded pointer-events-auto">
+
+        <!-- top part -->
+        <div class="flex justify-between items-center border-b border-red-50 px-3 mb-4">
+          <div>
+            <slot name="heading"></slot>
+          </div>
+          <div @click="close" class="p-2">
+            <div class="rotate-45 text-2xl cursor-pointer">+</div>
+          </div>
+        </div>
+
+        <!-- main content -->
+        <div class="px-2">
+          <slot />
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+/*
+The dialog will render as full page for phones in the below-sm sizes, and as a dialog with backdrop for larger screens.
+The detection is made using the backdrop - if it's displayed, we are not on such small screens.
+This method may not work too well on iOS devices because they delay zoom events, but it should only be a minor glitch.
+The alternative is to prevent user zooming via the meta tag, but we're avoiding this for now for accessibility reasons.
+ */
+import {onMounted, onUnmounted, ref} from "vue";
+
+const dialogBackdrop = ref<HTMLElement | null>(null);
+const dialogParent = ref<HTMLElement | null>(null);
+
+/** Functions we need to call to clean up before elements unmount */
+let cleanups: Function[] = []
+
+onMounted(() => {
+  const compactDialog = window.getComputedStyle(dialogBackdrop.value!).display == 'none';
+  if (compactDialog) {
+    // Solving the problem where users zoom in and cannot see the dialog anymore
+    // We start by aligning the dialog with the users' visual viewport & then 'prevent' the user from scrolling past
+    // the dialog by moving it
+    const resizeCallback = (ev: Event | undefined) => {
+      if (ev == undefined || dialogParent.value!.offsetTop > window.visualViewport.offsetTop) {
+        dialogParent.value!.style.top = `${window.visualViewport.offsetTop}px`;
+      }
+      dialogParent.value!.style.left = `${window.visualViewport.offsetLeft}px`;
+      dialogParent.value!.style.width = `${window.visualViewport.width}px`;
+    }
+    resizeCallback(undefined);
+    window.visualViewport.addEventListener("scroll", resizeCallback);
+    cleanups.push(() => window.visualViewport.removeEventListener("scroll", resizeCallback))
+  }
+})
+
+onUnmounted(() => {
+  cleanups.forEach(cleanup => cleanup());
+})
+
+/** Emits close when dialog wants to be closed */
+const emit = defineEmits(['close'])
+
+function close() {
+  emit('close');
+}
+
+</script>
--- src/frontend/src/components/classroom/PupilDialog.vue
+++ src/frontend/src/components/classroom/PupilDialog.vue
@@ -1,53 +1,44 @@
 <template>
-  <div>
-    <div @click="close" class="fixed left-0 right-0 top-0 bottom-0 bg-gray-300 opacity-80 z-20"></div>
-    <div class="fixed flex flex-col justify-center left-0 right-0 top-0 bottom-0 z-20">
+  <MobileFriendlyDialog @close="close">
+    <template v-slot:heading>
+      {{ props.pupil.pupil.name }}, {{ props.pupil.pupil.fromClass }}
+    </template>
 
-      <div class="bg-white mx-auto w-[65%] shadow-lg max-w-lg rounded">
-
-        <div class="flex justify-between items-center border-b border-red-50">
-          <h3 class="pl-3">{{ props.pupil.pupil.name }}, {{ props.pupil.pupil.fromClass }}</h3>
-          <div @click="close" class="p-2">
-            <div class="rotate-45 text-2xl cursor-pointer">+</div>
-          </div>
-        </div>
-
-        <div class="p-3" v-if="props.pupil.departure && props.pupil.departure.entireDay">
-          Otrok danes manjka cel dan.
-          <span v-if="props.pupil.departure.remark" class="block">
-            Opomba: {{props.pupil.departure.remark}}
-          </span>
-        </div>
-        <div v-else>
-          <div class="p-3" v-if="props.pupil.departure">
-            Otrok je odšel domov ob {{ formatDate(props.pupil.departure.time) }}
-          </div>
-          <div class="p-3" v-else-if="props.pupil.summon">
-            Otrok je bil poklican k vratom ob {{ formatDate(props.pupil.summon.time) }}
-          </div>
-          <div class="p-3" v-else>
-            <span v-if="props.pupil.departurePlan.leavesAlone">
-              Otrok gre domov sam ob {{ formatTime(props.pupil.departurePlan.time)}}.
-            </span>
-            <span v-else>
-              Po otroka pridejo predvidoma ob {{ formatTime(props.pupil.departurePlan.time) }}.
-            </span>
-          </div>
-
-          <p class="text-center space-y-3 md:space-x-3 mb-3">
-            <button @click="sendHome" v-if="showSendHomeButton()" class="bg-my-blue text-white rounded p-2 w-[150px]">Poslan/a domov</button>
-            <button @click="close" class="bg-sandy text-white rounded p-2 w-[150px]">Zapri</button>
-          </p>
-        </div>
-      </div>
+    <div class="p-3" v-if="props.pupil.departure && props.pupil.departure.entireDay">
+      Otrok danes manjka cel dan.
+      <span v-if="props.pupil.departure.remark" class="block">
+        Opomba: {{props.pupil.departure.remark}}
+      </span>
     </div>
-  </div>
+    <div v-else>
+      <div class="p-3" v-if="props.pupil.departure">
+        Otrok je odšel domov ob {{ formatDate(props.pupil.departure.time) }}
+      </div>
+      <div class="p-3" v-else-if="props.pupil.summon">
+        Otrok je bil poklican k vratom ob {{ formatDate(props.pupil.summon.time) }}
+      </div>
+      <div class="p-3" v-else>
+        <span v-if="props.pupil.departurePlan.leavesAlone">
+          Otrok gre domov sam ob {{ formatTime(props.pupil.departurePlan.time)}}.
+        </span>
+        <span v-else>
+          Po otroka pridejo predvidoma ob {{ formatTime(props.pupil.departurePlan.time) }}.
+        </span>
+      </div>
+
+      <p class="text-center space-y-3 md:space-x-3 mb-3">
+        <button @click="sendHome" v-if="showSendHomeButton()" class="bg-my-blue text-white rounded p-2 w-[150px]">Poslan/a domov</button>
+        <button @click="close" class="bg-sandy text-white rounded p-2 w-[150px]">Zapri</button>
+      </p>
+    </div>
+  </MobileFriendlyDialog>
 </template>
 <script lang="ts" setup>
 
 import {date2Time, stripSeconds} from "@/dateAndTime";
 import {pupilDeparted, requestPupilLeaveAlone, requestPupilSummonAck} from "@/pupil";
 import {DateTime} from "luxon";
+import MobileFriendlyDialog from "../MobileFriendlyDialog.vue";
 
 const formatDate = date2Time;
 const formatTime = stripSeconds;
